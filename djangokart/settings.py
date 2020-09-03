78831e114668ff045d6fe301f2423181b10da6dc
RJ-Gamer <rajatjog1294@gmail.com>
2020-09-03 18:49:53 +0530

    Pushing on heroku

--- djangokart/settings.py
+++ djangokart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yet_oldzfnz!s&hux8z0ri$#44%l&24093(d7-+_o0#(zgc6!='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
